2491ef7eb62c71219a4bfbbad631703c6386c21d
wonjunyoon630 <79366900+wonjunyoon630@users.noreply.github.com>
2021-03-16 21:18:51 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//윤원준
+const header = document.querySelector("#header");
+const headertopoffset =header.offsetTop
+
+window.addEventListener("scroll", e=>{
+    if(window.pageYOffset >= headertopoffset ) {
+        header.style.position="fixed";
+        header.style.top=0;
+        header.style.left=0;
+        header.style.right=0;
+    }
+    else{
+        header.style.position='';
+        header.style.top='';
+    }
+})
